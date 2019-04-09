5745cea6a7f0033bada880c2a23bda55f8c06630
goyalvaibhav25 <vaibhav.goyal2597@gmail.com>
2019-04-09 21:32:13 +0530

    Added Test Folder

---
+++ Test/start.bat
@@ -0,0 +1,3 @@
+start java -cp C:\Users\shubh\eclipse-workspace\quickstart\target\quickstart-0.0.1-SNAPSHOT-jar-with-dependencies.jar speechsdk.quickstart.JavaSoundRecorder
+timeout 3
+start java -cp C:\Users\shubh\eclipse-workspace\quickstart\target\quickstart-0.0.1-SNAPSHOT-jar-with-dependencies.jar speechsdk.quickstart.SpeechToTextConverter
